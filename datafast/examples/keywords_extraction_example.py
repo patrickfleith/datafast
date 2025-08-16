78e78e6300fa8339c3a9584d4a177bcd7803f2c5
Patrick <pfleith.pro@gmail.com>
2025-08-16 17:54:56 +0200

    example with keywords extraction

---
+++ datafast/examples/keywords_extraction_example.py
@@ -0,0 +1,66 @@
+"""
+Example script for generating a dataset using GenericPipelineDataset.
+This example uses the patrickfleith/FinePersonas-v0.1-100k-space-filtered dataset to generate tweets and CVs for different personas.
+"""
+
+import os
+from datafast.schema.config import GenericPipelineDatasetConfig
+from datafast.datasets import GenericPipelineDataset
+from datafast.llms import OpenAIProvider, GeminiProvider, OllamaProvider
+
+PROMPT_TEMPLATE = """I will give you a tweet.
+Generate a comma separated list of 3 keywords for the tweet. Avoid multi-word keywords.
+
+Here is the tweet:
+{tweet}
+
+Your response should be in {language} and formatted in valid JSON with {num_samples} entry and all required fields."""
+
+def main():
+    # 1. Define the configuration
+    config = GenericPipelineDatasetConfig(
+        hf_dataset_name="patrickfleith/generic_pipeline_test_dataset",
+        input_columns=["tweet"],        # Input data for generation
+        output_columns=["keywords"],    # Generated content columns
+        num_samples_per_prompt=1,       # Generate 1 set per persona
+        prompts=[PROMPT_TEMPLATE],
+        output_file="keywords_extraction_test_dataset.jsonl",
+        languages={"en": "English", "fr": "French", "es": "Spanish", "de": "German", "it": "Italian"}
+    )
+
+    # 2. Initialize LLM providers
+    providers = [
+        OpenAIProvider(
+            model_id="gpt-5-nano-2025-08-07",
+            ),
+        # AnthropicProvider(model_id="claude-3-5-haiku-latest"),
+        # GeminiProvider(model_id="gemini-2.5-flash-lite", rpm_limit=15),
+        # OllamaProvider(model_id="gemma3:4b"),
+    ]
+
+    # 3. Generate the dataset
+    dataset = GenericPipelineDataset(config)
+    num_expected_rows = dataset.get_num_expected_rows(providers)
+    print(f"\nExpected number of rows: {num_expected_rows}")
+    dataset.generate(providers)
+
+    # 4. Print results summary
+    print(f"\nGenerated {len(dataset.data_rows)} examples")
+    print(f"Results saved to {config.output_file}")
+
+    # 6. Optional: Push to HF hub
+    USERNAME = "username"  # <--- Your hugging face username
+    DATASET_NAME = "keywords_extraction_test_dataset"  # <--- Your hugging face dataset name
+    url = dataset.push_to_hub(
+        repo_id=f"{USERNAME}/{DATASET_NAME}",
+        seed=20250816,
+        shuffle=True,
+    )
+    print(f"\nDataset pushed to Hugging Face Hub: {url}")
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv("secrets.env")
+    main()
