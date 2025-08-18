73f0177634fd8c679863e3800da531ee3708fc9e
Patrick <pfleith.pro@gmail.com>
2025-08-18 19:25:23 +0200

    keyword extraction example

--- datafast/examples/keywords_extraction_example.py
+++ datafast/examples/keywords_extraction_example.py
@@ -24,18 +24,17 @@ def main():
         output_columns=["keywords"],    # Generated content columns
         num_samples_per_prompt=1,       # Generate 1 set per persona
         prompts=[PROMPT_TEMPLATE],
-        output_file="keywords_extraction_test_dataset.jsonl",
+        output_file="keywords_extraction_gemma3_runpod.jsonl",
         languages={"en": "English", "fr": "French", "es": "Spanish", "de": "German", "it": "Italian"}
     )
 
     # 2. Initialize LLM providers
     providers = [
-        OpenAIProvider(
-            model_id="gpt-5-nano-2025-08-07",
-            ),
-        # AnthropicProvider(model_id="claude-3-5-haiku-latest"),
-        # GeminiProvider(model_id="gemini-2.5-flash-lite", rpm_limit=15),
-        # OllamaProvider(model_id="gemma3:4b"),
+        OllamaProvider(
+            model_id="gemma3:27b-it-qat",
+            api_base="https://xxxxxxx-11434.proxy.runpod.net",
+            temperature=1
+        )
     ]
 
     # 3. Generate the dataset
@@ -50,7 +49,7 @@ def main():
 
     # 6. Optional: Push to HF hub
     USERNAME = "username"  # <--- Your hugging face username
-    DATASET_NAME = "keywords_extraction_test_dataset"  # <--- Your hugging face dataset name
+    DATASET_NAME = "keywords_extraction_gemma3_runpod"  # <--- Your hugging face dataset name
     url = dataset.push_to_hub(
         repo_id=f"{USERNAME}/{DATASET_NAME}",
         seed=20250816,
